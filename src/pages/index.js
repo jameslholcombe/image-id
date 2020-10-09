bf43f9eaf18b59c2b829196ee7a57f08a83b883c
James Holcombe <jameslholcombe@gmail.com>
2020-10-09 15:44:37 -0400

    Image preview working with tensorflow

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,41 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import Container from "../components/container"
+import UploadButton from "../components/UploadButton"
+
 import Button from "@material-ui/core/Button"
 
+// This is pinned at 2.4.0 because of a breaking change in 2.5.0
+require('@tensorflow/tfjs-backend-cpu')
+require("@tensorflow/tfjs-backend-webgl")
+
 export default function Home() {
-  require("@tensorflow/tfjs-backend-webgl")
+
+  const [selectedFile, setSelectedFile] = useState()
+  const [preview, setPreview] = useState()
+
+  const onSelectFile = e => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setSelectedFile(undefined)
+      return
+    }
+    setSelectedFile(e.target.files[0])
+  }
+
+  useEffect(() => {
+    if (!selectedFile) {
+      setPreview(undefined)
+      return
+    }
+
+    const objectUrl = URL.createObjectURL(selectedFile)
+    setPreview(objectUrl)
+
+    return () => URL.revokeObjectURL(objectUrl)
+  }, [selectedFile])
+
   const mobilenet = require('@tensorflow-models/mobilenet')
-  const image = require("../../static/coffee.jpg")
+  const cocoSsd = require('@tensorflow-models/coco-ssd')
 
   const idImage = async () => {
     const model = await mobilenet.load()
@@ -15,12 +44,25 @@ export default function Home() {
     console.log(predictions)
   }
 
+  const idObjects = async () => {
+    const model = await cocoSsd.load()
+    const img = document.getElementById('test')
+    const predictions = await model.detect(img)
+    console.log(predictions)
+  }
+
   return (
     <>
       <Container>
         <div>Home page</div>
-        <img src={image} id="test" alt="ID this" />
-        <Button onClick={idImage} variant="contained" color="primary">ID</Button>
+        <div>
+          <img src={preview} id="test" alt="ID this" />
+        </div>
+        <div>
+          <UploadButton onSelectFile={onSelectFile} />
+        </div>
+        <Button onClick={idImage} variant="contained" color="primary">ID Image</Button>
+        <Button onClick={idObjects} variant="contained" color="primary">ID Objects</Button>
       </Container>
     </>
   )
