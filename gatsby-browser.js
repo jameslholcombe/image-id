2c3e4d0af015f2cb638f744fdc7b527bc15e4ab0
James Holcombe <jameslholcombe@gmail.com>
2020-10-13 10:36:40 -0400

    Auth semi-working

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,35 @@
 import "./src/styles/global.css"
+import React from 'react'
+import { checkSession } from './src/utils/auth'
 
-import "firebase/auth"
-import "firebase/firestore"
-import "firebase/storage"
+class SessionCheck extends React.Component {
+  state = {
+    loading: true
+  }
+
+  construct() {
+    this.handleCheckSession = this.handleCheckSession.bind(this)
+  }
+
+  handleCheckSession = () => {
+    this.setState({ loading: false })
+  }
+
+  componentWillMount() {
+    checkSession(this.handleCheckSession);
+  }
+
+  render() {
+    return (
+      this.state.loading === false && (
+        <React.Fragment>{this.props.children}</React.Fragment>
+      )
+    )
+  }
+}
+
+export const wrapRootElement = ({ element }) => {
+  return(
+    <SessionCheck>{element}</SessionCheck>
+  )
+}
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,6 +2,11 @@ import auth0 from 'auth0-js'
 
 export const isBrowser = typeof window !== 'undefined';
 
+const tokens = {
+  idToken: false,
+  accessToken: false,
+}
+
 // Only instantiate Auth0 if we’re in the browser.
 const auth = isBrowser
   ? new auth0.WebAuth({
@@ -14,8 +19,28 @@ const auth = isBrowser
   : {};
 
 export const login = () => {
-  if (!isBrowser) {
-    return
-  }
   auth.authorize()
+}
+
+const setSession = (cb = () => {}) => (err, authResult) => {
+  if (err) {
+    if (err.error === 'login_required') {
+      login()
+    }
+  }
+
+  if (authResult && authResult.accessToken && authResult.idToken) {
+  tokens.idToken = authResult.idToken
+  tokens.accessToken = authResult.accessToken
+
+  cb()
+  }
+}
+
+export const checkSession = (callback) => {
+  auth.checkSession({}, setSession(callback))
+}
+
+export const handleAuthentication = () => {
+  auth.parseHash(setSession())
 }
