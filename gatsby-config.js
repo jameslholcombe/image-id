47074a19bf3ce8768219ca5e35ccfe068adc010e
James Holcombe <jameslholcombe@gmail.com>
2020-10-15 11:26:32 -0400

    Revert "Auth working initially"
    This reverts commit b557b16c67597daa22f92fa54d0afe2c2b0d17f6.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,4 +4,23 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-module.exports = {}
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "AIzaSyAuJIv0b65ovr6nwEwSy-QHi7tdXAof3UY",
+          authDomain: "image-id-2020.firebaseapp.com",
+          databaseURL: "https://image-id-2020.firebaseio.com",
+          projectId: "image-id-2020",
+          storageBucket: "image-id-2020.appspot.com",
+          messagingSenderId: "311144109271",
+          appId: "1:311144109271:web:f7e27dcd08705e79de8c92",
+          measurementId: "G-024RSV1EZS"
+        }
+      }
+    },
+  ],
+}
