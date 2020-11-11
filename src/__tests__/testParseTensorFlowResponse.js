24b37d87c36f9cbbcb345f0ba68d0a4af8ad5494
James Holcombe <jameslholcombe@gmail.com>
2020-11-11 09:59:59 -0500

    Parse helper and tests complete

--- src/__tests__/testParseTensorFlowResponse.js
+++ src/__tests__/testParseTensorFlowResponse.js
@@ -140,4 +140,84 @@ describe('test tensorflow response parsing', () => {
       }
     ]);
   });
+
+  it('both have duplicate keys and additional classes included', () => {
+    const mobileNetPredictions = [
+      {
+        className: 'box',
+        probability: 0.9,
+      },
+      {
+        className: 'box',
+        probability: 0.7,
+      },
+      {
+        className: 'person',
+        probability: 0.6,
+      },
+      {
+        className: 'lab coat, laboratory coat',
+        probability: 0.2630
+      },
+      {
+        className: 'desk',
+        probability: 0.1182
+      },
+      {
+        className: 'pajama, pyjama, pjs, jammies',
+        probability: 0.11657
+      }
+    ];
+
+    const cocoSsdPredictions = [
+      {
+        bbox: [
+          63.78,
+          84.936,
+          46.6442206799984,
+          99.47016835212708
+        ],
+        class: 'person',
+        score: 0.9526306390762329
+      },
+      {
+        bbox: [
+          89.8530021905899,
+          119.19286847114563,
+          68.42611277103424,
+          64.19280171394348
+        ],
+        class: 'person',
+        score: 0.7627212405204773
+      },
+      {
+        bbox: [
+          29.42897568643093,
+          111.69569194316864,
+          49.11157913506031,
+          80.37081360816956
+        ],
+        class: 'person',
+        score: 0.6902161240577698
+      }
+    ];
+
+    const parsedResult = parseTensorFlowResponse(
+      mobileNetPredictions,
+      cocoSsdPredictions
+    );
+
+    expect(parsedResult).toEqual([
+      {
+        count: 2,
+        class: 'boxes',
+        score: 0.8
+      },
+      {
+        count: 4,
+        class: 'people',
+        score: 0.75139200091362
+      }
+    ]);
+  });
 });
