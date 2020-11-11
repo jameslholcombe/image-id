63417724f2a6d7da46e10f655e9b8cc899b1928d
James Holcombe <jameslholcombe@gmail.com>
2020-11-11 09:32:10 -0500

    Aggregation working

--- src/__tests__/testParseTensorFlowResponse.js
+++ src/__tests__/testParseTensorFlowResponse.js
@@ -4,14 +4,17 @@ describe('test tensorflow response parsing', () => {
   it('both have response no same items', () => {
     const mobileNetPredictions = [
       {
+        count: 1,
         className: 'espresso',
         probability: 0.7745
       },
       {
+        count: 1,
         className: 'coffee mug',
         probability: 0.06242
       },
       {
+        count: 1,
         className: 'cup',
         probability: 0.0603
       }
@@ -37,12 +40,103 @@ describe('test tensorflow response parsing', () => {
 
     expect(parsedResult).toEqual([
       {
-        'class': 'cup',
-        'score': 0.9749
+        count: 1,
+        class: 'cup',
+        score: 0.9749
       },
       {
-        'class': 'espresso',
-        'score': 0.7745
+        count: 1,
+        class: 'espresso',
+        score: 0.7745
+      }
+    ]);
+  });
+
+  it('returns no valid prediction', () => {
+    const mobileNetPredictions = [
+      {
+        className: 'lakeside, lakeshore',
+        probability: 0.2039
+      },
+      {
+        className: 'barn',
+        probability: 0.05776
+      },
+      {
+        className: 'obelisk',
+        probability: 0.0550
+      }
+    ];
+
+    const cocoSsdPredictions = [];
+
+    const parsedResult = parseTensorFlowResponse(
+      mobileNetPredictions,
+      cocoSsdPredictions
+    );
+
+    expect(parsedResult).toEqual([]);
+  });
+
+  it('returns multiple CocoSsd predictions with same keys', () => {
+    const mobileNetPredictions = [
+      {
+        className: 'lab coat, laboratory coat',
+        probability: 0.2630
+      },
+      {
+        className: 'desk',
+        probability: 0.1182
+      },
+      {
+        className: 'pajama, pyjama, pjs, jammies',
+        probability: 0.11657
+      }
+    ];
+
+    const cocoSsdPredictions = [
+      {
+        bbox: [
+          63.78,
+          84.936,
+          46.6442206799984,
+          99.47016835212708
+        ],
+        class: 'person',
+        score: 0.9526306390762329
+      },
+      {
+        bbox: [
+          89.8530021905899,
+          119.19286847114563,
+          68.42611277103424,
+          64.19280171394348
+        ],
+        class: 'person',
+        score: 0.7627212405204773
+      },
+      {
+        bbox: [
+          29.42897568643093,
+          111.69569194316864,
+          49.11157913506031,
+          80.37081360816956
+        ],
+        class: 'person',
+        score: 0.6902161240577698
+      }
+    ];
+
+    const parsedResult = parseTensorFlowResponse(
+      mobileNetPredictions,
+      cocoSsdPredictions
+    );
+
+    expect(parsedResult).toEqual([
+      {
+        count: 3,
+        class: 'person',
+        score: 0.80185600121816,
       }
     ]);
   });
--- src/helpers/parseTensorflowResponse.js
+++ src/helpers/parseTensorflowResponse.js
@@ -2,6 +2,8 @@ const parseTensorFlowResponse = (
   mobileNetPredictions,
   cocoSsdPredictions
 ) => {
+
+  // Filter on score and change key names
   const filteredMobileNetPredictions = mobileNetPredictions.filter(prediction => {
     return prediction.probability > 0.3;
   }).map(prediction => {
@@ -11,15 +13,50 @@ const parseTensorFlowResponse = (
     };
   });
 
+  // Filter on score and remove unneeded key/value
   const filteredCocoSsdPredictions = cocoSsdPredictions
     .filter((prediction) => prediction.score > 0.3)
     .map(({bbox, ...remainingAttrs}) => remainingAttrs);
 
+  // Combine prediction arrays
   const combinedPredictions = [...filteredMobileNetPredictions, ...filteredCocoSsdPredictions];
 
-  combinedPredictions.sort((a, b) => parseFloat(b.score) - parseFloat(a.score));
+  // Array that will contain end result
+  const aggregatedPredictions = [];
 
-  return combinedPredictions;
+  // Array that is used for lookups for duplicate values
+  const distinctClass = [];
+
+  // Combine objects with same value for the key 'class'
+  combinedPredictions.map((prediction) => {
+    if (distinctClass.includes(prediction.class) === false) {
+      distinctClass.push(prediction.class);
+      aggregatedPredictions.push({
+        count: 1,
+        class: prediction.class,
+        score: prediction.score,
+      });
+    } else {
+      const existingAggregatedPrediction = aggregatedPredictions.filter(p => p.class === prediction.class)[0];
+      existingAggregatedPrediction.count = existingAggregatedPrediction.count + 1;
+      existingAggregatedPrediction.score = prediction.score + existingAggregatedPrediction.score;
+    }
+    return aggregatedPredictions;
+  });
+
+  // Average prediction.score for aggregated objects
+  aggregatedPredictions.map((prediction) => {
+    if (prediction.score > 1) {
+      return prediction.score = prediction.score / prediction.count;
+    } else {
+      return prediction.score;
+    }
+  });
+
+  // Sort prediction.score decreasing
+  aggregatedPredictions.sort((a, b) => parseFloat(b.score) - parseFloat(a.score));
+
+  return aggregatedPredictions;
 };
 
 export default parseTensorFlowResponse;
