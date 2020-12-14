b28cea01a111bb5b78bc901e564dbe3cdfef3a3b
James Holcombe <jameslholcombe@gmail.com>
2020-12-14 09:19:40 -0500

    Minor copy update

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,7 +7,7 @@ const Info = () => {
       <h3>Use Artificial Intelligence to identify images</h3>
       <div><strong>How it works:</strong></div>
       <ol>
-        <li>Upload any image by clicking the button to the right.</li>
+        <li>Upload any image</li>
         <li>
           Google's AI & Machine Learning engine, Tensorflow, will attempt to identify objects in the image.
         </li>
